bbd0582ea8e5791bcf1e6009eb393857bf2dbc0f
Ajay Shrivas <ajay.shrivas@himanshusofttech.com>
2023-10-12 10:55:05 +0530

    This is simple way in data send
    This is a react file we can send data on node js use state and string function

---
+++ Contact.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+const Contact = () => {
+
+    const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = { 
+      firstName,
+      lastName,
+      email,
+    };
+
+    // Send the form data to the server using the fetch method
+    fetch('http://192.168.29.23:8000/reg', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+
+
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="First Name"
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Last Name"
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+
+};
+export default Contact;
